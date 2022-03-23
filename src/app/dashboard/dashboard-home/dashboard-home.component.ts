903567e61c3b97639324d67492e1868069ace751
RADP3 <dit.radp3.cmn@nabard.org>
2022-03-23 20:54:11 +0530

    Material Table changes for MIS status

--- src/app/dashboard/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/dashboard-home/dashboard-home.component.ts
@@ -4,6 +4,23 @@ import { Title } from '@angular/platform-browser';
 import { NGXLogger } from 'ngx-logger';
 import { AuthenticationService } from 'src/app/core/services/auth.service';
 
+
+export interface PeriodicElement {
+  return: string;
+  position: number;
+  dueDate: number;
+  dateOfSubmission: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, return: 'MIS-1',      dueDate: 3,  dateOfSubmission: 'H'},
+  {position: 2, return: 'MIS-2(CTP)', dueDate: 3,  dateOfSubmission: 'He'},
+  {position: 3, return: 'MIS-2A',     dueDate: 31, dateOfSubmission: 'Li'},
+  {position: 4, return: 'MIS-2B',     dueDate: 3,  dateOfSubmission: 'Be'},
+];
+
+
+
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
@@ -11,6 +28,9 @@ import { AuthenticationService } from 'src/app/core/services/auth.service';
 })
 export class DashboardHomeComponent implements OnInit {
   currentUser: any;
+
+  displayedColumns: string[] = ['position', 'return', 'dueDate', 'dateOfSubmission' ,'submissionStatus' , 'links'];
+  dataSource = ELEMENT_DATA;
 
   constructor(private notificationService: NotificationService,
     private authService: AuthenticationService,
