9730f08ad6899b100ffa961c7d61837dc9f19949
RADP3 <dit.radp3.cmn@nabard.org>
2022-03-25 15:24:56 +0530

    TS : added getters in the constant file

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -9,32 +9,83 @@ export class Constants
     private readonly UAT_URL: string = "http://uat.nabard.org:8080";
     private readonly PRD_URL: string = "http://prd.nabard.org:8080";
     
-    private readonly MIS_BASE: string = "/ddmmis/api";
-    private readonly DROPDOWNS: string = "/v1/prerequisites/dropdowns";
-    private readonly MIS1: string = "/mis1/v1/";
-    private readonly MIS2: string = "/mis2/v1/";
+    private readonly _MIS_BASE: string = "/ddmmis/api";
+    private readonly _DROPDOWNS: string = "/v1/prerequisites/dropdowns";
+    private readonly _MIS1: string = "/mis1/v1/";
+    private readonly _MIS2: string = "/mis2/v1/";
     
-    private readonly IMDV1: string = "/imdv/p1";
-    private readonly IMDV2: string = "/imdv/p2";
-    private readonly VU: string = "/vu";
-    private readonly MOCO: string = "/moco";
-    private readonly MCA: string = "/mca";
-    private readonly WSP: string = "/wsp";
-    private readonly BTOR: string = "/btor";
+    private readonly _IMDV1: string = "/imdv/p1";
+    private readonly _IMDV2: string = "/imdv/p2";
+    private readonly _VU: string = "/vu";
+    private readonly _MOCO: string = "/moco";
+    private readonly _MCA: string = "/mca";
+    private readonly _WSP: string = "/wsp";
+    private readonly _BTOR: string = "/btor";
     
     // Operations to be performed
-    private readonly CREATE: string = "/create";
-    private readonly UPDATE: string = "/update";
+    private readonly _CREATE: string = "/create";
+    private readonly _UPDATE: string = "/update";
     
-    private readonly ALL: string = "/all";
-    private readonly VIEW: string = "/view";
-    private readonly FETCHBYFIELDS: string = "/FetchByFields";
-    private readonly REPORTS: string = "/reports";
+    private readonly _ALL: string = "/all";
+    private readonly _VIEW: string = "/view";
+    private readonly _FETCHBYFIELDS: string = "/FetchByFields";
+    private readonly _REPORTS: string = "/reports";
     
     constructor() { }
 
     public get ROOT_URL(): string {
         return this._ROOT_URL;
+    }
+    public get MIS_BASE(): string {
+        return this._MIS_BASE;
+    }
+    public get DROPDOWNS(): string {
+        return this._DROPDOWNS;
+    }
+    public get MIS1(): string {
+        return this._MIS1;
+    }
+    public get MIS2(): string {
+        return this._MIS2;
+    }
+    public get IMDV1(): string {
+        return this._IMDV1;
+    }
+    public get IMDV2(): string {
+        return this._IMDV2;
+    }
+    public get VU(): string {
+        return this._VU;
+    }
+    public get MOCO(): string {
+        return this._MOCO;
+    }
+    public get MCA(): string {
+        return this._MCA;
+    }
+    public get WSP(): string {
+        return this._WSP;
+    }
+    public get BTOR(): string {
+        return this._BTOR;
+    }
+    public get CREATE(): string {
+        return this._CREATE;
+    }
+    public get UPDATE(): string {
+        return this._UPDATE;
+    }
+    public get ALL(): string {
+        return this._ALL;
+    }
+    public get VIEW(): string {
+        return this._VIEW;
+    }
+    public get FETCHBYFIELDS(): string {
+        return this._FETCHBYFIELDS;
+    }
+    public get REPORTS(): string {
+        return this._REPORTS;
     }
     
     public set ROOT_URL(v: IpUrl | string) {
